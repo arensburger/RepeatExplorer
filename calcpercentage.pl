90f4c9c574a4a550238ff9b45c704bbd81a306a1
Peter <arensburger@gmail.com>
2014-07-15 14:29:59 +0200

    changed calcpercentage, it now takes

--- calcpercentage.pl
+++ calcpercentage.pl
@@ -1,40 +1,50 @@
 #!/usr/bin/perl
 # calculates the percentage TE occupancy
 use strict;
+use Text::CSV;
+use Getopt::Long;
 
-my %occupancy;
-my $seqinclusters; # total number of sequences in clusters
-
-open (INPUT, $ARGV[0]) or die;
-while (my $line = <INPUT>) {
-	if ($line =~ /\S+\t(\d+)\t(\S*)/) {
-		my $abundance = $1;
-		my $category = $2;
-
-		if ($category eq "") {
-			$occupancy{"not-classified"} += $abundance;
-		}
-		else {
-			$occupancy{$category} += $abundance;
-		}
-		$seqinclusters += $abundance;
-	}
-	else {
-		warn ("WARNING, not reading line $line");
-	}
+my $manual_annotation_file; # manual annotation file csv
+my $REdirectory; # Repeat Explorer output directory name
+my $outputfile; # ouput file
+GetOptions(
+	'm:s'   => \$manual_annotation_file,
+	'r:s'	=> \$REdirectory,
+	'o:s'	=> \$outputfile,
+);
+unless ($manual_annotation_file and $REdirectory and $outputfile) {
+	print "usage: perl calcpercentage\n\t-m <REQUIRED, manual annotation file as a CSV file>\n\t-r <REQUIRED, Repeat Explorer output directory>\n\t-o <REQUIRED, output file name>\n";
 }
 
-foreach my $cat (keys %occupancy) {
-	print "$cat\t$occupancy{$cat}";
-	if (exists $ARGV[1]) {
-		my $percentage = $occupancy{$cat}/$ARGV[1];
-		print "\t$percentage\n";
-	}
+### Global variables ###
+my %readnumbers; # short cluster name as key number of reads per cluster as value
+my %categorienumbers; # repeat category names as key and cluster read abundance as value
+
+## read the number of reads in each cluster
+#my @clusterdir = split (" ", `find $REdirectory/seqClust/clustering/clusters/ -name "dir_*"`);
+#foreach my $cluster (@clusterdir) {
+#	my $totalreads_cluster = `grep -v ">" $cluster/reads.fas -c`;
+#	print "$cluster\t$totalreads_cluster\n";
+#}
+#exit;
+
+## read the manual annotation and populate the %categorienumbers hash
+my $csv = Text::CSV->new({ sep_char => ',' }); # create the CSV object
+open(my $data, '<', $manual_annotation_file) or die "Could not open '$manual_annotation_file' $!\n";
+while (my $line = <$data>) {
+	chomp $line;
+	if ($csv->parse($line)) {
+		my @fields = $csv->fields();
+		my $category = $fields[0];
+		if (length $fields[1] > 0) {
+			$category .= "/" . $fields[1];
+		}
+		$categorienumbers{$category} += 1;
+  	} 
 	else {
-		print "\n";
-	}
+      		warn "Line could not be parsed: $line\n";
+  	}
 }
-if (exists $ARGV[1]) {
-	my $percentageincluster = $seqinclusters/$ARGV[1];
-	print "Num sequences in clusters\t$seqinclusters\t$percentageincluster\n";
-}	
+
+exit;
+
